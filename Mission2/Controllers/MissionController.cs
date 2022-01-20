cce21037e2f7c4914fbf6a312fd4607f7a496c01
mina-m-chia <90296893+mina-m-chia@users.noreply.github.com>
2022-01-19 22:21:48 -0700

    MVC model

---
+++ Mission2/Controllers/MissionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission2.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Mission2.Controllers
+{
+    public class MissionController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission2/Models/GradeCalculatorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission2.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Range(0,100)]
+        public int Assignments { get; set; }
+
+        [Range(0, 100)]
+        public int Project { get; set; }
+
+        [Range(0, 100)]
+        public int Quizzes { get; set; }
+
+        [Range(0, 100)]
+        public int Exams { get; set; }
+
+        [Range(0, 100)]
+        public int Intex { get; set; }
+    }
+}
